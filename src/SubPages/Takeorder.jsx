f1bdf9c6dd0d74b8c5a9cdbaaf9188d150c9d445
arhamansari11 <arhamansari454647@gmail.com>
2024-11-11 22:57:25 +0500

    Changings in the Take order page

--- src/SubPages/Takeorder.jsx
+++ src/SubPages/Takeorder.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { db } from "../Config/Firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 const Takeorder = () => {
-  return (
-    <div>Takeorder</div>
-  )
-}
+  const [categories, setCategories] = useState([]);
 
-export default Takeorder
+  const fetchCategories = async () => {
+    const categoryCollectionRef = collection(db, "Category");
+    const data = await getDocs(categoryCollectionRef);
+    setCategories(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Available Categories</h2>
+      <ul>
+        {categories.map((category) => (
+          <li key={category.id}>
+            <strong>{category.category}</strong> - {category.description}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Takeorder;
